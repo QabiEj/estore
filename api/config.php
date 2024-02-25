6623fbaf381cd9b30b80dfde31f5fa34f371f65c
QabiEj <101940223+QabiEj@users.noreply.github.com>
2024-02-25 21:41:43 +0000

    Add omnipay/omnipay dependency and configure allow-plugins in composer.json. Update database connection in config.php.

--- api/config.php
+++ api/config.php
@@ -13,13 +13,28 @@ define('PAYPAL_CANCEL_URL', 'http://localhost/e-commerce/cancel.php');
 define('PAYPAL_CURRENCY', 'EUR'); // set your currency here
  
 // Connect with the database
-$db = new mysqli('localhost', 'username', 'password', 'databasename'); 
- 
-if ($db->connect_errno) {
-    die("Connect failed: ". $db->connect_error);
+$host = getenv('POSTGRES_HOST');
+$db   = getenv('POSTGRES_DATABASE');
+$user = getenv('POSTGRES_USER');
+$pass = getenv('POSTGRES_PASSWORD');
+$port = getenv('POSTGRES_PORT');
+$dsn = "pgsql:host=$host;port=$port;dbname=$db;user=$user;password=$pass";
+
+try{
+    // create a PostgreSQL database connection
+    $conn = new PDO($dsn);
+    
+    // display a message if connected to the PostgreSQL successfully
+    if($conn){
+        echo "Connected to the <strong>$db</strong> database successfully!";
+    }
+}catch (PDOException $e){
+    // report error message
+    echo $e->getMessage();
 }
  
 $gateway = Omnipay::create('PayPal_Rest');
 $gateway->setClientId(CLIENT_ID);
 $gateway->setSecret(CLIENT_SECRET);
-$gateway->setTestMode(true); //set it to 'false' when go live?>
+$gateway->setTestMode(true); //set it to 'false' when go live
+?>
