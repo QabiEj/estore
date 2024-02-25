599cc12b8f8b1bfdb2e3a080b66daf3c9cf8ab07
ShabanEjupi <101940223+ShabanEjupi@users.noreply.github.com>
2024-02-25 21:04:21 +0100

    first commit

---
+++ api/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    // HTML code for the home page
+    echo "<html>";
+    echo "<head>";
+    echo "<title>Home Page</title>";
+    echo "</head>";
+    echo "<body>";
+    echo "<h1>Welcome to the Home Page</h1>";
+    echo "<p>This is the content of the home page.</p>";
+    echo "</body>";
+    echo "</html>";
+?>
