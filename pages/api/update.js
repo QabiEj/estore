a921a5f357590d2ac9d613c2974096e54ec00d0d
QabiEj <101940223+QabiEj@users.noreply.github.com>
2024-02-29 10:07:33 +0000

    Remove unnecessary files and directories

--- pages/api/update.js
+++
@@ -1,6 +0,0 @@
-// pages/api/update.js
-export default async function handler(req, res) {
-    // Your update logic here
-
-    res.status(200).json({ status: 'success' });
-}
--- pages/cancel.js
+++
@@ -1,8 +0,0 @@
-// pages/cancel.js
-export default function Cancel() {
-    return (
-        <div>
-            <h3>User cancelled the payment.</h3>
-        </div>
-    );
-}
