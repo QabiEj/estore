36e37569b833b80da4f228004f0206d106015800
QabiEj <101940223+QabiEj@users.noreply.github.com>
2024-04-01 01:16:50 +0000

    veq shk diqka

--- artisan-bakery/resources/js/site.js
+++
@@ -1,9 +0,0 @@
-import Alpine from "alpinejs";
-import focus from '@alpinejs/focus'
-import mediumZoom from "medium-zoom";
-
-window.Alpine = Alpine;
-Alpine.start();
-Alpine.plugin(focus)
-
-mediumZoom("[data-zoomable]");
